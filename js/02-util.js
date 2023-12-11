e1edc520975cf1dacb6edfef59db314e285d2254
alex <alex.zeiss@gmx.net>
2023-12-11 10:53:59 +0100

    Add task 2

---
+++ js/02-util.js
@@ -0,0 +1,17 @@
+/** 02 Executing Functions Directly & Indirectly */
+function double(n) {
+  return n * 2;
+}
+
+function transform(n, fn) {
+  if (typeof fn !== "function") return;
+  return fn(n);
+}
+
+console.log(transform(2, double));
+
+console.log(
+  transform(4, function (n) {
+    return n * 2;
+  })
+);
