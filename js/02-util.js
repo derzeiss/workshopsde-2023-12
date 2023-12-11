f3235b48d5e5a86b183a801989a045920899f1fc
alex <alex.zeiss@gmx.net>
2023-12-11 11:39:11 +0100

    Add task 3 and 4

--- js/02-util.js
+++ js/02-util.js
@@ -1,17 +1,45 @@
 /** 02 Executing Functions Directly & Indirectly */
-function double(n) {
-  return n * 2;
-}
-
-function transform(n, fn) {
-  if (typeof fn !== "function") return;
-  return fn(n);
-}
-
-console.log(transform(2, double));
-
-console.log(
-  transform(4, function (n) {
+const task2 = () => {
+  function double(n) {
     return n * 2;
-  })
-);
+  }
+
+  function transform(n, fn) {
+    if (typeof fn !== "function") return;
+    return fn(n);
+  }
+
+  console.log(transform(2, double));
+
+  console.log(
+    transform(4, function (n) {
+      return n * 2;
+    })
+  );
+};
+
+/** 03 map() */
+const task3 = () => {
+  const transformOToObjects = (nums) => nums.map((n) => ({ val: n }));
+
+  console.log(transformOToObjects([1, 2, 3, 4]));
+};
+
+/** 04 Arrow Functions and reduce */
+function task4() {
+  const doubleAndSquareAndSum = (numArr) =>
+    numArr
+      .map((n) => n * 2)
+      .map((n) => Math.pow(n, 2))
+      .reduce((sum, n) => sum + n, 0);
+
+  const doubleAndSquareAndSumOneLine = (numArr) =>
+    numArr.reduce((sum, n) => sum + Math.pow(n * 2, 2), 0);
+
+  // Performance vs. readability
+
+  console.log(doubleAndSquareAndSum([2, 4, 5]));
+  console.log(doubleAndSquareAndSumOneLine([2, 4, 5]));
+}
+
+task4();
