7fb6a37d3fd97e562eb3c5a89efab51f48ff5b4c
alex <alex.zeiss@gmx.net>
2023-12-15 10:52:11 +0100

    Add react task 3

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,9 +1,11 @@
 import { AppHeader } from "./components/AppHeader";
+import { ExampleBook } from "./components/ExampleBook";
 
 function App() {
   return (
     <div className="app">
       <AppHeader />
+      <ExampleBook />
     </div>
   );
 }
