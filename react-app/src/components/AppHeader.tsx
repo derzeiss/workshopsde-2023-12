80852e12ef6dce1697ff8341dfb0f787cf5be56c
alex <alex.zeiss@gmx.net>
2023-12-18 13:06:47 +0100

    Add ThemeContext

--- react-app/src/components/AppHeader.tsx
+++ react-app/src/components/AppHeader.tsx
@@ -1,10 +1,14 @@
+import { useContext } from "react";
 import logo from "../img/logo.png";
+import { ThemeContext } from "../domain/theme/ThemeContext";
 
 export const AppHeader = () => {
+  const { primaryColor } = useContext(ThemeContext);
+
   return (
     <div className="app-header">
       <img src={logo} alt="An emoji-styled monkey with glasses" />
-      <h1>Bookmonkey</h1>
+      <h1 style={{ color: primaryColor }}>Bookmonkey</h1>
     </div>
   );
 };
--- react-app/src/components/BookListItem.tsx
+++ react-app/src/components/BookListItem.tsx
@@ -1,20 +1,22 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Book, cx } from "workshops-de_shared";
 import { Hideable } from "./Hideable";
 import { LikeCounter } from "./LikeCounter";
+import { ThemeContext } from "../domain/theme/ThemeContext";
 
 interface BookListItemProps {
   book: Book;
 }
 
 export const BookListItem: React.FC<BookListItemProps> = ({ book }) => {
+  const { primaryColor } = useContext(ThemeContext);
   const [likes, setLikes] = useState(0);
   const isFree = book.price === "$0.00";
 
   return (
     <div className={cx("book-list-item", { "book-list-item_free": isFree })}>
       <img src={book.cover} alt={`Cover of ${book.title}`} />
-      <h2>
+      <h2 style={{ color: primaryColor }}>
         {likes >= 5 && <span className="icon_entry">⭐️ </span>}
         {isFree && <span>💰 </span>}
         {book.title}
