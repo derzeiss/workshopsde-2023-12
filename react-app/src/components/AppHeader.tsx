193936aa4f72920767f755d726643b474e2b76e1
alex <alex.zeiss@gmx.net>
2023-12-19 11:43:28 +0100

    Add nav links

--- react-app/src/components/AppHeader.tsx
+++ react-app/src/components/AppHeader.tsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import logo from "../img/logo.png";
 import { ThemeContext } from "../domain/theme/ThemeContext";
 import { ThemeEditor } from "../domain/theme/ThemeEditor";
+import { NavLink } from "react-router-dom";
 
 export const AppHeader = () => {
   const { primaryColor } = useContext(ThemeContext);
@@ -11,6 +12,10 @@ export const AppHeader = () => {
       <img src={logo} alt="An emoji-styled monkey with glasses" />
       <h1 style={{ color: primaryColor }}>Bookmonkey</h1>
       <ThemeEditor />
+      <nav>
+        <NavLink to="/books">Books</NavLink>
+        <NavLink to="/about">About</NavLink>
+      </nav>
     </div>
   );
 };
