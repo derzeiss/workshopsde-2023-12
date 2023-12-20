d0d82a64a3ca067f2e4063d9cdef68bcb8bcd02a
alex <alex.zeiss@gmx.net>
2023-12-20 13:33:41 +0100

    Make shared package testable

--- shared/src/util/formatPrice.ts
+++ shared/src/util/formatPrice.ts
@@ -1,6 +1,6 @@
 export const formatPrice = (amount: number) => {
   return Intl.NumberFormat(navigator.language, {
-    currency: "USD",
-    style: "currency",
+    currency: 'USD',
+    style: 'currency',
   }).format(amount);
 };
