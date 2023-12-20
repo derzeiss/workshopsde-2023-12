3fe1b6bcda6f1840a726da484b707fddc05cddb7
alex <alex.zeiss@gmx.net>
2023-12-20 10:35:11 +0100

    Use formValues object

--- react-app/src/screens/BookEditScreen.tsx
+++ react-app/src/screens/BookEditScreen.tsx
@@ -1,23 +1,29 @@
-import { FormEvent, useEffect, useState } from "react";
+import { ChangeEventHandler, FormEvent, useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import { Book } from "workshops-de_shared";
 import { useBook } from "../domain/book/useBook";
 
 export const BookEditScreen = () => {
   const { isbn } = useParams<{ isbn: string }>();
-  const [title, setTitle] = useState("");
   const { book, state } = useBook(isbn);
+  // using own variable here to prevent mutating book
+  const [formValues, setFormValues] = useState<Book>({} as Book);
 
   const handleSubmit = (ev: FormEvent<HTMLFormElement>) => {
     ev.preventDefault();
-    console.log("submit", title);
+    console.log("submit", formValues?.title);
   };
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (ev) =>
+    setFormValues({ ...formValues, [ev.target.name]: ev.target.value });
 
   useEffect(() => {
     if (!book) return;
-    setTitle(book.title);
+    setFormValues({ ...book });
   }, [book]);
 
-  if (!book || state === "loading") return <h2>Loading...</h2>;
+  if (state === "initial" || state === "loading" || !formValues.title)
+    return <h2>Loading...</h2>;
 
   return (
     <div className="book-edit-screen">
@@ -27,14 +33,24 @@ export const BookEditScreen = () => {
           id="title"
           type="text"
           name="title"
-          value={title}
-          onChange={(ev) => setTitle(ev.target.value)}
+          value={formValues.title}
+          onChange={handleChange}
         />
 
-        {title}
+        <label htmlFor="author">Author</label>
+        <input
+          id="author"
+          type="text"
+          name="author"
+          value={formValues.author}
+          onChange={handleChange}
+        />
+
         <button type="submit" className="m-top">
           <span>💾</span>Save
         </button>
+
+        <pre>{JSON.stringify(formValues, null, 2)}</pre>
       </form>
       <Link to={`/books/${isbn}`}>
         <button className="secondary m-top">Cancel</button>
